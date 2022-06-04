4587ff92cea89c69bad5ebed497afc0d81473734
GabrielMs18 <gabriel-montes32@outlook.es>
2022-06-03 22:23:32 -0500

    Producto perfil controller

--- Modelo/Productos.php
+++ Modelo/Productos.php
@@ -1,30 +1,30 @@
 <?php
 require_once("Conexion.php");
-class Admin
+class Productos
 {
 	public function ObtenerTodos()
 	{
 		$conexion = new Conexion;
-		$Admin = $conexion->consultar('admin');
+		$Admin = $conexion->consultar('productos');
 		return $Admin;
 	}
 	public function nuevo($datos)
 	{
 		$conexion = new Conexion;
-		$Admin = $conexion->insertar('admin', $datos);
+		$Admin = $conexion->insertar('productos', $datos);
 		return $Admin;
 	}
 	public function Guardar($datos, $filtro)
 	{
 		$conexion = new Conexion;
-		$Admin = $conexion->actualizar('admin', $datos, $filtro);
+		$Admin = $conexion->actualizar('productos', $datos, $filtro);
 		return $Admin;
 	}
 
 	public function ObtenerFiltro($filtro)
 	{
 		$conexion = new Conexion;
-		$Admin = $conexion->consultarFiltro('admin', $filtro);
+		$Admin = $conexion->consultarFiltro('productos', $filtro);
 		return $Admin;
 	}
 }
--- Vistas/Mantenimiento/Perfil_Supervisor.php
+++ Vistas/Mantenimiento/Perfil_Supervisor.php
@@ -2,114 +2,81 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-    <link rel="stylesheet" href="../css/style.css">
-    <title>Perfil</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../../css/botones.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <link rel="stylesheet" href="../css/style.css">
+  <title>Perfil</title>
 
+  <script type='text/javascript'>
+    
+    function cargarcontrolador() {
+
+      $.post("../../Controlador/ProductosController.php", {
+        'opcion': 'consultar'
+      }, respuesta);
+    }
+
+    function respuesta(arg) {
+      $("#datos tbody").append(arg);
+    }
+
+    function editar(codigo) {
+      document.location.href = "../update.php?id_admin=" + codigo;
+    }
+    window.onload = cargarcontrolador;
+  </script>
 </head>
 
 <body
-    background="https://blakesguam.com/wp-content/uploads/2016/08/photodune-6207464-geometric-polygon-abstract-background-l-4.jpg"
-    class="cuerpo">
+  background="https://blakesguam.com/wp-content/uploads/2016/08/photodune-6207464-geometric-polygon-abstract-background-l-4.jpg"
+  class="cuerpo">
 
-    <div class="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark" style="width: 280px; height: 970px;">
-        <a href="../Index-Login-Admin.php"
-            class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-            <img src="../../imagenes/1.png" width="50" height="50" alt="">
-            <span class="fs-4">Farmacia</span>
+  <div class="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark" style="width: 280px; height: 970px;">
+    <a href="../index.html" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+      <img src="../../imagenes/1.png" width="50" height="50" alt="">
+      <span class="fs-4">Farmacia</span>
+    </a>
+    <hr>
+    <ul class="nav nav-pills flex-column mb-auto">
+      <li class="nav-item">
+        <a href="Opciones.php" class="nav-link text-white" aria-current="page">
+          <svg class="bi me-2" width="16" height="16">
+          </svg>
+          <i class="bi bi-house"></i>
+          Home
         </a>
-        <hr>
-        <ul class="nav nav-pills flex-column mb-auto">
-            <li class="nav-item">
-                <a href="Opciones.php" class="nav-link text-white" aria-current="page">
-                    <svg class="bi me-2" width="16" height="16">
-                    </svg>
-                    <i class="bi bi-house"></i>
-                    Home
-                </a>
+      </li>
+    </ul>
+  </div>
 
-            </li>
-        </ul>
-    </div>
-    <main class="edi">
-    <table class="table table-hover">
-  <thead>
-    <tr>
-      <th scope="col">Type</th>
-      <th scope="col">Column heading</th>
-      <th scope="col">Column heading</th>
-      <th scope="col">Column heading</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr class="table-active">
-      <th scope="row">Active</th>
-      <td>Column content</td>
-      <td>Column content</td>
-      <td>Column content</td>
-    </tr>
-    <tr>
-      <th scope="row">Default</th>
-      <td>Column content</td>
-      <td>Column content</td>
-      <td>Column content</td>
-    </tr>
-    <tr class="table-primary">
-      <th scope="row">Primary</th>
-      <td>Column content</td>
-      <td>Column content</td>
-      <td>Column content</td>
-    </tr>
-    <tr class="table-secondary">
-      <th scope="row">Secondary</th>
-      <td>Column content</td>
-      <td>Column content</td>
-      <td>Column content</td>
-    </tr>
-    <tr class="table-success">
-      <th scope="row">Success</th>
-      <td>Column content</td>
-      <td>Column content</td>
-      <td>Column content</td>
-    </tr>
-    <tr class="table-danger">
-      <th scope="row">Danger</th>
-      <td>Column content</td>
-      <td>Column content</td>
-      <td>Column content</td>
-    </tr>
-    <tr class="table-warning">
-      <th scope="row">Warning</th>
-      <td>Column content</td>
-      <td>Column content</td>
-      <td>Column content</td>
-    </tr>
-    <tr class="table-info">
-      <th scope="row">Info</th>
-      <td>Column content</td>
-      <td>Column content</td>
-      <td>Column content</td>
-    </tr>
-    <tr class="table-light">
-      <th scope="row">Light</th>
-      <td>Column content</td>
-      <td>Column content</td>
-      <td>Column content</td>
-    </tr>
-    <tr class="table-dark">
-      <th scope="row">Dark</th>
-      <td>Column content</td>
-      <td>Column content</td>
-      <td>Column content</td>
-    </tr>
-  </tbody>
-</table>
-    </main>
+
+
+  <div class="edi">
+    <h1 class="text-center">Productos</h1>
+    <button type="button" class="btn btn-outline-dark"><a href="../new.html">Nuevo Producto</a></button>
+    <br />
+    <table class="table" id="datos">
+      <thead>
+        <tr>
+          <th scope="col">id</th>
+          <th scope="col">nombre</th>
+          <th scope="col">descripcion</th>
+          <th scope="col">f_fabricacion</th>
+          <th scope="col">f_caducidad</th>
+          <th scope="col">precio</th>
+        </tr>
+      </thead>
+      <tbody>
+      </tbody>
+    </table>
+  </div>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+    crossorigin="anonymous"></script>
 </body>
 
 </html>
