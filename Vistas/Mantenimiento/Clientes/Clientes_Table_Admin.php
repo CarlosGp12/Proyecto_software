841ff30d016a76cc689e3488634fce3caf60dc84
GabrielMs18 <gabriel-montes32@outlook.es>
2022-08-21 23:36:08 -0500

    Dashboard en todas las tablas admin y opciones

--- Vistas/Mantenimiento/Clientes/Clientes_Table_Admin.php
+++ Vistas/Mantenimiento/Clientes/Clientes_Table_Admin.php
@@ -1,29 +1,22 @@
 <?php
 
-session_start();
-if(isset($_SESSION['username']))
-{
-}
+// session_start();
+// if(isset($_SESSION['username']))
+// {
+// }
 
-    if(!isset($_SESSION['rol'])){
-        header('location: ../../login.php');
-    }else{
-        if($_SESSION['rol'] != 1){
-            header('location: ../../login.php');
-        }
-    }
+//     if(!isset($_SESSION['rol'])){
+//         header('location: ../../login.php');
+//     }else{
+//         if($_SESSION['rol'] != 1){
+//             header('location: ../../login.php');
+//         }
+//     }
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-  <link rel="stylesheet" href="../css/style.css">
 
   <title>Perfil</title>
   <script type='text/javascript' src="../../js/jquery-1.7.1.min.js"> </script>
@@ -54,27 +47,9 @@ if(isset($_SESSION['username']))
   </script>
 </head>
 
-<body background="https://blakesguam.com/wp-content/uploads/2016/08/photodune-6207464-geometric-polygon-abstract-background-l-4.jpg" class="cuerpo">
+  <?php require '../../../includes/dash2.php';?>
+    <section class="page-content">
 
-  <div class="d-flex">
-    <div class="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark" style="width: 280px; height: 970px;">
-      <a href="../../index.html" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-        <span class="fs-4">Farmacia</span>
-      </a>
-      <hr>
-      <ul class="nav nav-pills flex-column mb-auto">
-        <li class="nav-item">
-          <a href="../Opciones.php" class="nav-link text-white" aria-current="page">
-            <svg class="bi me-2" width="16" height="16">
-            </svg>
-            <i class="bi bi-house"></i>
-            Home
-          </a>
-        </li>
-      </ul>
-    </div>
-
-    <div class="edi">
       <h1 class="text-center">Clientes</h1>
       <button type="button" class="btn btn-outline-dark"><a href="new.php">Nuevo Cliente</a></button>
       <br />
@@ -91,9 +66,7 @@ if(isset($_SESSION['username']))
         <tbody>
         </tbody>
       </table>
-    </div>
-  </div>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    </section>
 </body>
 
 </html>
--- Vistas/Mantenimiento/Opciones.php
+++ Vistas/Mantenimiento/Opciones.php
@@ -1,114 +1,28 @@
 <?php
-session_start();
-if(isset($_SESSION['username']))
-{
-}
+// session_start();
+// if(isset($_SESSION['username']))
+// {
+// }
 
-    if(!isset($_SESSION['rol'])){
-        header('location: ../login.php');
-    }else{
-        if($_SESSION['rol'] != 1){
-            header('location: ../login.php');
-        }
-    }
-
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Perfil</title>
-    <link rel="stylesheet" href="../../css/botones.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
-    <script type='text/javascript' src="../../js/jquery-1.7.1.min.js"> </script>
-</head>
-
-<body background="https://blakesguam.com/wp-content/uploads/2016/08/photodune-6207464-geometric-polygon-abstract-background-l-4.jpg" class="cuerpo">
+//     if(!isset($_SESSION['rol'])){
+//         header('location: ../login.php');
+//     }else{
+//         if($_SESSION['rol'] != 1){
+//             header('location: ../login.php');
+//         }
+//     }
+require '../../includes/dash.php';
+// ?>
 
 
-    <div class="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark" style="width: 280px; height: 970px;">
-        <a href="../index.html" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-            <span class="fs-4">Farmacia</span>
-        </a>
-        <hr>
-        <ul class="nav nav-pills flex-column mb-auto">
-            <li class="nav-item">
-                <a href="" class="nav-link text-white  active" aria-current="page">
-                    <svg class="bi me-2" width="16" height="16">
-                        <use xlink:href="#home"></use>
-                    </svg>
-                    Home
-                </a>
-            </li>
-            <li>
-                <a href="Articulo/Supervisor_Table_Admin.php" class="nav-link text-white">
-                    <svg class="bi me-2" width="16" height="16">
-                        <use xlink:href="#speedometer2"></use>
-                    </svg>
-                    Articulo
-                </a>
-            </li>
-            <li>
-                <a href="Clientes/Clientes_Table_Admin.php" class="nav-link text-white">
-                    <svg class="bi me-2" width="16" height="16">
-                        <use xlink:href="#speedometer2"></use>
-                    </svg>
-                    Clientes
-                </a>
-            </li>
-            <li>
-                <a href="Proovedor/Proovedor_Table_Admin.php" class="nav-link text-white">
-                    <svg class="bi me-2" width="16" height="16">
-                        <use xlink:href="#table"></use>
-                    </svg>
-                    Proovedor
-                </a>
-            </li>
-            <li>
-                <a href="Supervisor/Supervisor_Table.php" class="nav-link text-white">
-                    <svg class="bi me-2" width="16" height="16">
-                        <use xlink:href="#table"></use>
-                    </svg>
-                    Supervisor
-                </a>
-                <a href="Vendedor/Vendedor_Table.php" class="nav-link text-white">
-                    <svg class="bi me-2" width="16" height="16">
-                        <use xlink:href="#table"></use>
-                    </svg>
-                    Vendedor
-                </a>
-            </li>
-            <i> 
-                Usted a iniciado como: <?php echo $_SESSION['username'] ?> 
-            </i>
-        </ul>
-        <hr>
-        <div class="dropdown">
-            <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
-                <img src="../../imagenes/admin.png" alt="" width="32" height="32" class="rounded-circle me-2">
-                <strong><?php  echo $admin; ?></strong>
-            </a>
-            <ul class="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1">
-
-                <li><a class="dropdown-item" href="http://localhost/Pasarela/Vistas/Proyecto/Logout.php?logout">Sign out</a></li>
-            </ul>
-        </div>
-    </div>
 
 
     <div class="edi">
         <main class="menu">
             <div class="container py-4">
 
-                    <footer class=" pt-3 mt-4 text-muted border-top">
-                        © 2021
-                    </footer>
-               
+
+
         </main>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
