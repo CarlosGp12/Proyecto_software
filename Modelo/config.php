fb98bdfc2958203f4030d27a1143aae38a2991b1
Danielw121 <85859929+Danielw121@users.noreply.github.com>
2022-08-26 20:56:30 -0500

    Parametros de conexion y encriptacion de datos

---
+++ Modelo/config.php
@@ -0,0 +1,10 @@
+
+<?php
+define("KEY", "develoteca");
+define("COD", "AES-128-ECB");
+
+define("SERVIDOR", "localhost");
+define("USUARIO", "root");
+define("PASSWORD", "");
+define("BD", "farmacia");
+?>
