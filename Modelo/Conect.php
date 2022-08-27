6c98390450df877d2de6e0a404fc3b3806f1dba6
Danielw121 <85859929+Danielw121@users.noreply.github.com>
2022-08-26 20:55:08 -0500

    Conexion para el carrito

---
+++ Modelo/Conect.php
@@ -0,0 +1,15 @@
+
+<?php
+$servidor = "mysql:dbname=" . BD . ";host=" . SERVIDOR;
+
+try {
+    $pdo = new PDO(
+        $servidor,
+        USUARIO,
+        PASSWORD,
+        array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")
+    );
+} catch (PDOException $e) {
+    echo "<script>alert('ERROR...')</script>";
+}
+?>
