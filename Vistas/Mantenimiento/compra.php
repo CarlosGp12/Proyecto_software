f3ac4b0415dc561146b892af41308666fcf38b7e
CarlosGp <carlos200012-@hotmail.com>
2022-08-31 12:13:38 -0500

    formulario compra

---
+++ Vistas/Mantenimiento/compra.php
@@ -0,0 +1,28 @@
+<?php
+    require '../../includes/dash.php';
+?>
+
+<section class="page-content">
+  <div class="contenedor_formulario">
+    <h1 class="titulo">Pedir productos</h1>
+    <Form>
+        <div>
+            <label for="">Proveedor</label>
+            <select  name="" id="">
+                <option value=""></option>
+            </select>
+        </div>
+        <div>
+        <label for="">Articulo</label>
+            <input class="campo" type="text">
+        </div>
+        <div>
+            <label for="">stock</label>
+            <input class="campo" type="number">
+        </div>
+        <button class="botones enviar">Enviar</button>
+    </Form>
+  </div>
+</section>  
+</body>
+</html>
